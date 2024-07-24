b31198cdfa0b58a5371828c7e8fbde100f60bc68
luca-vigano <luca.vigano@hotmail.com>
2024-07-24 16:44:47 +0200

    button work in progres

--- Unit 2/Settimana 3/Giorno 3/assets/script.js
+++ Unit 2/Settimana 3/Giorno 3/assets/script.js
@@ -17,27 +17,28 @@ const getLibrary = function () {
 }
 
 
-const generateCArd= function(arrayOfBook){
-    const pickrow=document.getElementById("row")
+const generateCArd = function (arrayOfBook) {
+    const pickrow = document.getElementById("row")
     arrayOfBook.forEach(book => {
-        const newCol=document.createElement("div")
+        const newCol = document.createElement("div")
         newCol.classList.add("col-6", "col-mid-3", "col-lg-2",)
-        const newDiv=document.createElement("div")
+        const newDiv = document.createElement("div")
         newDiv.classList.add("card", "bg-dark", "text-light", "border", "border-light")
-        const newImg=document.createElement("img")
+        const newImg = document.createElement("img")
         newImg.classList.add("card-img-top")
-        newImg.src= book.img
-        const newBodyDiv=document.createElement("div")
+        newImg.src = book.img
+        const newBodyDiv = document.createElement("div")
         newBodyDiv.classList.add("card-body")
-        const newTitle=document.createElement("h5")
+        const newTitle = document.createElement("h5")
         newTitle.classList.add("card-title")
-        newTitle.innerText=book.title
-        const newP=document.createElement("p")
+        newTitle.innerText = book.title
+        const newP = document.createElement("p")
         newP.classList.add("card-text")
         newP.innerText = book.price
-        const secondP=document.createElement("p")
+        const secondP = document.createElement("p")
         secondP.classList.add("card-text")
         secondP.innerText = book.category
+        
         pickrow.appendChild(newCol)
         newCol.appendChild(newDiv)
         newDiv.appendChild(newImg)
@@ -45,7 +46,17 @@ const generateCArd= function(arrayOfBook){
         newBodyDiv.appendChild(newTitle)
         newBodyDiv.appendChild(newP)
         newBodyDiv.appendChild(secondP)
+
+        const deleteButton = document.createElement("button")
+        deleteButton.type = "button"
+        deleteButton.classList.add("btn", "btn-sm", "btn-danger")
+        deleteButton.innerText = "DELETE"
+        deleteButton.addEventListener("click", function (e) {
+            e.target.closest(".col").classList.add("d-none")
+            newBodyDiv.appendChild(deleteButton)
+        })
     });
+
 }
 
 getLibrary()
