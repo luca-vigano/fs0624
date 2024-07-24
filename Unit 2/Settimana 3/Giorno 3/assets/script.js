3678d1ab575ef644aa9eee4f4f00fc2688f546af
luca-vigano <luca.vigano@hotmail.com>
2024-07-24 15:58:06 +0200

    libreria ok

---
+++ Unit 2/Settimana 3/Giorno 3/assets/script.js
@@ -0,0 +1,51 @@
+const getLibrary = function () {
+    fetch("https://striveschool-api.herokuapp.com/books")
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error;
+            }
+        })
+        .then((bookList) => {
+            generateCArd(bookList)
+            console.log(bookList)
+        })
+        .catch((error) => {
+            console.log("Errore", error)
+        })
+}
+
+
+const generateCArd= function(arrayOfBook){
+    const pickrow=document.getElementById("row")
+    arrayOfBook.forEach(book => {
+        const newCol=document.createElement("div")
+        newCol.classList.add("col-6", "col-mid-3", "col-lg-2",)
+        const newDiv=document.createElement("div")
+        newDiv.classList.add("card", "bg-dark", "text-light", "border", "border-light")
+        const newImg=document.createElement("img")
+        newImg.classList.add("card-img-top")
+        newImg.src= book.img
+        const newBodyDiv=document.createElement("div")
+        newBodyDiv.classList.add("card-body")
+        const newTitle=document.createElement("h5")
+        newTitle.classList.add("card-title")
+        newTitle.innerText=book.title
+        const newP=document.createElement("p")
+        newP.classList.add("card-text")
+        newP.innerText = book.price
+        const secondP=document.createElement("p")
+        secondP.classList.add("card-text")
+        secondP.innerText = book.category
+        pickrow.appendChild(newCol)
+        newCol.appendChild(newDiv)
+        newDiv.appendChild(newImg)
+        newDiv.appendChild(newBodyDiv)
+        newBodyDiv.appendChild(newTitle)
+        newBodyDiv.appendChild(newP)
+        newBodyDiv.appendChild(secondP)
+    });
+}
+
+getLibrary()
