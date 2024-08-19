bd21c44f595b5c89e2718b831b37c408fa24d998
luca-vigano <luca.vigano@hotmail.com>
2024-08-19 15:33:45 +0200

    Delete Unit 3/Prova React directory

--- Unit 3/Prova React/first-app/src/components/ImageComponent.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class ImageComponent extends Component {
-    render () {
-        return <img src={this.props.src} alt={this.props.alt}></img>
-    }
-}
-
-export default ImageComponent
