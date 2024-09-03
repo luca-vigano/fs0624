62e72aeccf18b237a720b7079b08f61e71b12f3a
luca-vigano <luca.vigano@hotmail.com>
2024-09-03 10:29:02 +0200

    fix

--- Unit 3/settimana 3/giorno 1/reduxjob/src/redux/actions/index.js
+++ Unit 3/settimana 3/giorno 1/reduxjob/src/redux/actions/index.js
@@ -3,14 +3,14 @@ export const REMOVE_FROM_FAVORITES = "REMOVE_FROM_FAVORITES";
 
 export const addToFavoriteAction = (company) => {
   return {
-    type: "ADD_TO_FAVORITES",
+    type: ADD_TO_FAVORITES,
     payload: company,
   };
 };
 
 export const removeFromFavorites = (i) => {
   return {
-    type: "REMOVE_FROM_FAVORITES",
+    type: REMOVE_FROM_FAVORITES,
     payload: i,
   };
 };
