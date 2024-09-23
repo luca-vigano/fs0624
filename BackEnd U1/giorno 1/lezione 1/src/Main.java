6f9dfeb3c16f58143694b241192d209ccc79fddc
luca-vigano <luca.vigano@hotmail.com>
2024-09-23 17:30:16 +0200

    prova

--- BackEnd U1/giorno 1/lezione 1/src/Main.java
+++ BackEnd U1/giorno 1/lezione 1/src/Main.java
@@ -1,13 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 6;
-        int xy = x * y;
-        System.out.print("Risultato " + xy);
+    }
+
+    public static int moltiplica(int x, int y) {
+        return x * y;
+    }
+
+    public static String concatena(String name, int age) {
+        return "Ciao" + name + "ho" + age + "anni";
+    }
+
+    public static String[] inserisciInArray(String[] arr, String str) {
+        String[] arrStr = new String[6];
+        arrStr[0] = arr[0];
+        arrStr[1] = arr[1];
+        arrStr[2] = str;
+        arrStr[3] = arr[2];
+        arrStr[4] = arr[3];
+        arrStr[5] = arr[4];
+
+        return arrStr;
     }
 
 
-    String[] arrStr = new String[4];
-    String str1 = "sono una stringa";
-    // String[]
 }
