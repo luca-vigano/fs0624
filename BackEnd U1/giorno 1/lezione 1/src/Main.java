afd293d9983b6d78caf6bd249d2c845615dfa5c7
luca-vigano <luca.vigano@hotmail.com>
2024-09-23 17:06:45 +0200

    prova

---
+++ BackEnd U1/giorno 1/lezione 1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int xy = x * y;
+        System.out.print("Risultato " + xy);
+    }
+
+
+    String[] arrStr = new String[4];
+    String str1 = "sono una stringa";
+    // String[]
+}
