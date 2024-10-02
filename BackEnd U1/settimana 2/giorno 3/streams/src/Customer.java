4da247838c2bfc2ea760f646536440ddde573566
luca-vigano <luca.vigano@hotmail.com>
2024-10-02 14:56:31 +0200

    es1

--- BackEnd U1/settimana 2/giorno 3/streams/src/Customer.java
+++ BackEnd U1/settimana 2/giorno 3/streams/src/Customer.java
@@ -12,4 +12,28 @@ public class Customer {
         this.name = name;
         this.tier = tier;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
+
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
 }
--- BackEnd U1/settimana 2/giorno 3/streams/src/Product.java
+++ BackEnd U1/settimana 2/giorno 3/streams/src/Product.java
@@ -11,4 +11,46 @@ public class Product {
         this.category = category;
         this.price = price;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
