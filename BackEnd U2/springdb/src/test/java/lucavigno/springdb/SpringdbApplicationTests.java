6e361e2bd282baf19725d0dc078dd7771288793a
luca-vigano <luca.vigano@hotmail.com>
2024-10-24 11:18:32 +0200

    up 2

---
+++ BackEnd U2/springdb/src/test/java/lucavigno/springdb/SpringdbApplicationTests.java
@@ -0,0 +1,13 @@
+package lucavigno.springdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
